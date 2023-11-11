f06af45cca25472618b92f014142a22fdf2fbeb5
Vic Samoylik <v.samojlik@gmail.com>
2023-11-11 20:18:18 +0300

    refactor: add service div to Header layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 function Header() {
   return (
     <header className="header">
-      <div className="header__row">
-        <h1 className="header__logo">Vic's Guidebook of Countries</h1>
-        <button className="header__icon">Dark Mode</button>
+      <div className="container">
+        <div className="header__row">
+          <h1 className="header__logo">Vic's Guidebook of Countries</h1>
+          <button className="header__icon">Dark Mode</button>
+        </div>
       </div>
     </header>
   );
