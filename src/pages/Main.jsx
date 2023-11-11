edf08eab8db83c9cb16d1ca4f8df3ba565b23aaf
Vic Samoylik <v.samojlik@gmail.com>
2023-11-11 21:39:55 +0300

    feat: add search and filter component boilerplates

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
 import Header from '../components/Header';
+import Search from '../components/Search';
+import Filter from '../components/Filter';
 
 function Main() {
   return (
     <>
       <Header />
+      <div className="container">
+        <div className="search-filter">
+          <div className="search-filter__row">
+            <Search />
+            <Filter />
+          </div>
+          <div className="countries"></div>
+        </div>
+      </div>
     </>
   );
 }
