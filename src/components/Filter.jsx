09bea3703983a7544fbf3eabb5e0cc8c5ea9331f
Vic Samoylik <v.samojlik@gmail.com>
2023-11-13 12:03:57 +0300

    feat: add basic layout to Filter Component

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 function Filter() {
+  const [countriesFilter, setCountriesFilter] = React.useState('');
+  console.log(countriesFilter);
   return (
-    <div>
-      filter
-    </div>
-  )
+    <section className="filter">
+      <select
+        value={countriesFilter}
+        onChange={(event) => setCountriesFilter(event.target.value)}
+        name="countries-filter"
+      >
+        <option value="" disabled selected>
+          Filter by Region
+        </option>
+        <option value="all">All</option>
+        <option value="africa">Africa</option>
+        <option value="america">America</option>
+        <option value="asia">Asia</option>
+        <option value="europe">Europe</option>
+        <option value="oceania">Oceania</option>
+      </select>
+    </section>
+  );
 }
 
-export default Filter
+export default Filter;
