b6366357ee653b5d3878ab5f8a47daa5f1874e6c
Vic Samoylik <v.samojlik@gmail.com>
2023-11-21 19:53:00 +0300

    feat: add theme context and header theme toggle logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ThemeContext } from './Layout';
+import { checkTheme } from '../functions/checktheme';
 
 import logoLight from '../assets/logo-light-theme.svg';
 import logoDark from '../assets/logo-dark-theme.svg';
@@ -7,16 +9,24 @@ import moonIcon from '../assets/moon-icon.svg';
 import sunIcon from '../assets/sun-icon.svg';
 
 function Header() {
+  const { theme, changeTheme } = React.useContext(ThemeContext);
   return (
     <header className="header">
       <div className="container">
         <div className="header__row">
           <Link to="/" className="header__logo">
-            <img src={logoLight} alt="Vic's Guidebook of Countries logo" />
+            <img
+              src={checkTheme(theme, logoDark, logoLight)}
+              alt="Vic's Guidebook of Countries logo"
+            />
           </Link>
-          <button className="header__light-toggle light-toggle">
-            <img className="light-toggle__icon" src={moonIcon} alt="Moon Icon" />
-            <p className="light-toggle__text">{'Dark Theme'}</p>
+          <button className="header__light-toggle light-toggle" onClick={() => changeTheme()}>
+            <img
+              className="light-toggle__icon"
+              src={checkTheme(theme, sunIcon, moonIcon)}
+              alt="Moon Icon"
+            />
+            <p className="light-toggle__text">{checkTheme(theme, 'Light Theme', 'Dark Theme')}</p>
           </button>
         </div>
       </div>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import useLocalStorage from 'use-local-storage';
 
 import Header from './Header';
 
-function Layout() {
+const ThemeContext = React.createContext();
+
+export default function Layout() {
+  const [theme, setTheme] = useLocalStorage('theme' ? 'dark' : 'ligh');
+  const changeTheme = () => setTheme(theme === 'dark' ? 'light' : 'dark');
   return (
-    <div className="wrapper">
-      <Header />
-      <main className="main">
-        <div className="container">
-          <Outlet />
-        </div>
-      </main>
-    </div>
+    <ThemeContext.Provider value={{ theme, setTheme, changeTheme }}>
+      <div className="wrapper">
+        <Header />
+        <main className="main">
+          <div className="container">
+            <Outlet />
+          </div>
+        </main>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
-export default Layout;
+export { ThemeContext };
